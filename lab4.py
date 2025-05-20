3e72205531723bbde9d139e170228ac46928c4cd
Shimozuki1 <mr.kopper2004@gmail.com>
2025-05-20 19:30:20 +0400

    lab4.py

--- lab4.py
+++ lab4.py
@@ -45,4 +45,4 @@ def process_file(text):
         converted_num = num.replace('.', ',')
         print(converted_num)
 
-process_file(r'C:\Users\UserPC\Desktop\input\input.txt')
+process_file(r'C:\Users\UserPC\Desktop\text\text.txt')
